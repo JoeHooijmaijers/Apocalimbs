2cf0c0c272d949d3088c913eb3237a11f9b5434c
Maxvdiepen <32391947+Maxvdiepen@users.noreply.github.com>
2019-02-15 11:08:31 +0100

    Enemy movement
    Obstacles added, navigation baked and added a script for the enemy to follow the player if the player enters a certain radius.

---
+++ Assets/AIMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    Transform player;
+    NavMeshAgent nav;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        //if()
+        //{
+        //    nav.SetDestination(player.position);
+        //}
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform == player)
+        {
+            nav.SetDestination(player.position);
+        }
+    }
+}
