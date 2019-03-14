0f0e85ab1c33f5a48d5b646fd61b19235d46fc72
Maxvdiepen <32391947+Maxvdiepen@users.noreply.github.com>
2019-03-15 00:07:45 +0100

    Lunge
    Added lunge animation and code, only part not working is the position transform of the Gremlin during the lunge attack. Also made minor updates to the movement and detection code to make them work properly again.

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -6,41 +6,70 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    Transform target;
+    NavMeshAgent nav;
+    Animator animator;
     public float awareness = 6f;
     public float vision = 10f;
-    public float turnSpeed = 5f;
-    float distance = Vector3.Distance(target.position, transform.position);
-
-    Transform target;
-    NavMeshAgent agent;
+    public float turnSpeed = 3f;
+    public float timeAirborne = 1f;
+    private float distance;
 
     // Start is called before the first frame update
     void Awake()
     {
-        agent = GetComponent<NavMeshAgent>();
+        nav = GetComponent<NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
-    {             
+    {
+        distance = Vector3.Distance(target.position, transform.position);
+       
         if (distance <= awareness)
         {
-            inRange();
-            
+            Debug.Log("InRange");
+            InRange(distance);
         }
     }
 
-    private void inRange()
+    private void InRange(float Distance)
     {
-        agent.SetDestination(target.position);
-        if (distance <= agent.stoppingDistance)
+        Vector3 LastSeen = new Vector3(target.position.x, target.position.y, target.position.z);
+        nav.SetDestination(LastSeen);
+        FaceTarget();
+        if (Distance <= nav.stoppingDistance)
         {
-            //lunge
-
+            //Lunge
+            animator.SetTrigger("InLungeRange");
+            Debug.Log("Lekker Lungen");
             //Face target
-            FaceTarget();
         }
+    }
+
+    private void Lunge()
+    {
+        transform.position = Vector3.Slerp(transform.position, new Vector3(target.position.x, target.position.y, target.position.z), Time.deltaTime * timeAirborne);
+        
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.transform == target)
+        {
+            Vector3 LastSeen = new Vector3(target.position.x, target.position.y, target.position.z);
+            nav.SetDestination(LastSeen);
+            InRange(distance);
+        }
+        //Vector3 position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        //Vector3 cov = this.transform.forward;
+
+        //if (Physics.Raycast(position, cov, vision))
+        //{
+        //    Debug.DrawLine(position, cov, Color.red);
+        //}
     }
 
     private void FaceTarget()
@@ -55,16 +84,5 @@ public class EnemyController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, awareness);
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        Vector3 position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-        Vector3 cov = this.transform.forward;
-
-        if (Physics.Raycast(position, cov, vision))
-        {
-            Debug.DrawLine(position, cov, Color.red);
-        }
     }
 }
--- Assets/Scripts/Hitbox.cs
+++ Assets/Scripts/Hitbox.cs
@@ -7,7 +7,7 @@ public class Hitbox : MonoBehaviour
     public int damage;
     private void OnTriggerEnter(Collider col)
     {
-        if(col.tag == "Enemy")
+        if(col.tag == "Enemy" || col.tag == "Player")
         {
             col.GetComponent<Combat>().TakeDamage(damage);
             Debug.Log(col);
