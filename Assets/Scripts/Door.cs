2c2c5da3a53e32a10206514593ae28a6efa2b186
JoeHooijmaijers <JoeHooijmaijers@users.noreply.github.com>
2019-03-15 09:30:27 +0100

    Fence Commit
    Contains a fence, animation, 2 scripts

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject door;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OpenDoor()
+    {
+
+    }
+}
