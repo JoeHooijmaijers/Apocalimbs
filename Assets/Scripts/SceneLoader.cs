a892b34b492ea675111500bd1928e4fe11f73b3c
JoeHooijmaijers <JoeHooijmaijers@users.noreply.github.com>
2019-04-14 20:41:47 +0100

    Main Menu Commit
    Created Main Menu Scene
    Added Assets for TextMeshPro
    Created a temporary background

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class sceneLoader : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
+    public string sceneToLoad;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            SceneManager.LoadScene("Level1Whitebox", LoadSceneMode.Single);
+            SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
         }
     }
 }
