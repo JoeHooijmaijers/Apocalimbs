9590ad9f3e616f6a0e2c1a454445a706bce1fe69
JoeHooijmaijers <JoeHooijmaijers@users.noreply.github.com>
2019-04-24 22:49:40 +0200

    Boss Door Commit
    Added a boss door
    Added a trigger for said door

---
+++ Assets/slidingDoor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slidingDoor : MonoBehaviour
+{
+    [SerializeField] private Vector3 closedPos;
+    [SerializeField] private Vector3 openPos;
+
+    [SerializeField] private float slidingSpeed;
+
+    public void OpenDoor()
+    {
+        StartCoroutine(IEOpenDoor());
+    }
+
+    public void CloseDoor()
+    {
+        StartCoroutine(IECloseDoor());
+    }
+
+    IEnumerator IEOpenDoor()
+    {
+        while(transform.position != openPos)
+        {
+            transform.position = transform.position = Vector3.MoveTowards(transform.position, openPos, Time.deltaTime * slidingSpeed);
+            yield return null;
+        }
+    }
+
+    IEnumerator IECloseDoor()
+    {
+        while (transform.position != closedPos)
+        {
+            transform.position = transform.position = Vector3.MoveTowards(transform.position, closedPos, Time.deltaTime * slidingSpeed);
+            yield return null;
+        }
+    }
+
+    
+}
